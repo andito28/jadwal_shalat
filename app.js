e6ab3e5ff1350670460e224284aade42633c926d
andito28 <andito763@gmail.com>
2021-03-09 21:42:07 +0800

    create file app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+function index(){
+
+    let app = document.querySelector("#app");
+    let h3 = document.createElement("h3");
+    h3.textContent = "Jadwal Shalat";
+
+    app.appendChild(h3);
+}
+
+index();
+
+
+
