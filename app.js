36becb12eb11c391ace8ca58a8fe238978d36fa7
andito28 <andito763@gmail.com>
2021-03-11 22:34:18 +0800

    finish

--- app.js
+++ app.js
@@ -8,22 +8,24 @@ function jadwal_shalat(latitude,longitude){
         let today = date.getDate() - 1;
         let data = response.data[today].timings;
 
-        let table = document.createElement("table");
         let app = document.querySelector("#app");
+        let table = document.createElement("table");
         let tBody = document.createElement("tbody");
 
-        console.log(data);
+
         for(i in data){
 
             let row = tBody.insertRow();
             let name = row.insertCell(0);
             let time = row.insertCell(1);
             name.textContent = i;
-            time.textContent = data[i];
-            tBody.appendChild(row);   
+            time.textContent = data[i]; 
+            
         }
+
         table.appendChild(tBody);
         app.appendChild(table);
+
     });
 }
 
@@ -46,11 +48,26 @@ function userLocation(){
 }
 
 function index(){
-
+    var tanggallengkap = new String();
+    var namahari = ("Minggu Senin Selasa Rabu Kamis Jumat Sabtu");
+    namahari = namahari.split(" ");
+    var namabulan = ("Januari Februari Maret April Mei Juni Juli Agustus September Oktober November Desember");
+    namabulan = namabulan.split(" ");
+    var tgl = new Date();
+    var hari = tgl.getDay();
+    var tanggal = tgl.getDate();
+    var bulan = tgl.getMonth();
+    var tahun = tgl.getFullYear();
+    tanggallengkap = namahari[hari] + ", " +tanggal + " " + namabulan[bulan] + " " + tahun;
+    
     let app = document.querySelector("#app");
     let h3 = document.createElement("h3");
-    h3.textContent = "Jadwal Shalat";
+    let span = document.createElement("span");
+   
+    h3.textContent = "JADWAL SHALAT";
+    span.textContent = tanggallengkap;
 
+    app.appendChild(span);
     app.appendChild(h3);
 
     userLocation();
